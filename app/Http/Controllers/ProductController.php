28d0d9318340836de3fad06dfe9a77b9872ff3cd
linith2244github <linithseang2244@gmail.com>
2026-02-11 10:23:46 +0700

    add r

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -24,9 +24,28 @@ class ProductController extends Controller
     }
 
     public function store(Request $request){
-        return response()->json([
-            'status' => 200,
-            'message'=> 'Product created successfully'
-        ], 201);
+        $validator = Validator::make($request->all(),[
+            "name"=> "required|min:4",
+            "price"=> "required",
+            "qty"=> "required",
+        ]);
+        if($validator->passes()){
+            $product = new Product();
+            $product->name = $request->input('name');
+            $product->description = $request->input('description');
+            $product->price = $request->input('price');
+            $product->qty = $request->input('qty');
+            $product->save();
+            return response()->json([
+                'status' => 200,
+                'message'=> 'Product created successfully'
+            ], 201);
+        }else{
+            return response()->json([
+                'status'=> 500,
+                'message'=> "Please config errors",
+                "errors" => $validator->errors()
+            ], 500);
+        }
     }
 }
